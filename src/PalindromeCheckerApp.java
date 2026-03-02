ff5b8fee5b0dec1a763d7f05804b33f55d1a4db9
Lakshmi98496 <kottadilakshmi897@gmail.com>
2026-03-02 15:05:10 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,19 +1,9 @@
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-
-        // UC3: Reverse-based palindrome check
-        String input = "madam";
-        String reversed = "";
-
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
-        }
-
-        if (input.equals(reversed)) {
-            System.out.println("Palindrome");
-        } else {
-            System.out.println("Not a Palindrome");
-        }
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("System initialized successfully");
     }
 }
